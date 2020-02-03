f6f843c267663c082e37b7df4744802a4102be81
Alexi Maschas <alexi.maschas@gmail.com>
2020-02-03 18:00:44 -0500

    running from rex staging

--- test-es.py
+++ test-es.py
@@ -1,20 +1,28 @@
 from elasticsearch_dsl import connections, Search
+# import logging
 import os
 
-# from memory_profiler import profile
+from memory_profiler import profile
 
 ES_HOST = os.environ.get("ES_HOST", "")
 JOBS_INDEX = os.environ.get("JOBS_INDEX", "")
 
-connections.create_connection(hosts=[ES_HOST])
+connections.create_connection(
+    hosts=[ES_HOST],
+    use_ssl=False,
+    verify_certs=False
+)
 
-# @profile
+# es_logger = logging.getLogger('elasticsearch')
+# es_logger.setLevel(logging.INFO)
+
+@profile
 def my_func():
     for x in range(1000):
-        print(x)
-    #     s = Search()
-    #     s = s[:100]
-    #     r = s.execute()
-    # return r
+        s = Search(index=JOBS_INDEX)
+        s = s[:10]
+        r = s.execute()
+    return r
 
-my_func()
+for x in range(3):
+    my_func()
